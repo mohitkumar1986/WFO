82c13d59c7ff57b16e5ce627e42fc777e6d3363c
I323772 <mohit.kumar02@sap.com>
2020-05-27 04:16:39 +0000

    test commit

---
+++ webapp/controller/SelectLocation.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller,MessageToast,JSONModel) {
+	"use strict";
+
+	return Controller.extend("WSR.WorkspaceReservation.controller.SelectLocation", {
+		onInit: function () {
+		
+		}
+
+	});
+});
