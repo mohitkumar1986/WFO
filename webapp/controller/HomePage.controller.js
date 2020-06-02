3aa2266a5838518e7b1541c139ce045666897e97
KatieBebb <bebbingtonkatie@gmail.com>
2020-06-02 08:56:14 +0100

    Update HomePage.controller.js

--- webapp/controller/HomePage.controller.js
+++ webapp/controller/HomePage.controller.js
@@ -7,12 +7,29 @@ sap.ui.define([
 
 	return Controller.extend("WSR.WorkspaceReservation.controller.View1", {
 		onInit: function () {
+			/* This code was causing issues as the view was being instantiated again with
+				duplicate ID 
+				Now on load the list is not filtered but when user presses next/last week it IS filtered*/
 			var oBinding = this.byId("myList").getBinding("items");
+			if(oBinding !== undefined){
 			oBinding.filter(new sap.ui.model.Filter("week", "EQ", "Week1"));
+}
 		},
-		onShowHello : function () {
-			//var sRecipient = this.getView().getModel().getProperty("/recipient");
-			MessageToast.show("Dialog to Search Locations");
+		onSelectLocationButton: function () {
+			//	var sRecipient = this.getView().getModel().getProperty("/recipient");
+			//	MessageToast.show("sRecipient "+ sRecipient);
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("RouteView2");
+		},
+		onPressNext: function () {
+			var oBinding = this.byId("myList").getBinding("items");
+		//	MessageToast.show("oBinding " + oBinding);
+			oBinding.filter(new sap.ui.model.Filter("week", "EQ", "Week2"));
+		},
+		onPressNext: function () {
+			var oBinding = this.byId("myList").getBinding("items");
+		//	MessageToast.show("oBinding " + oBinding);
+			oBinding.filter(new sap.ui.model.Filter("week", "EQ", "Week2"));
 		},
 		onPressNext: function () {
 			var oBinding = this.byId("myList").getBinding("items");
